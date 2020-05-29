e7e5aa9e7f17ae4a1e07109ea33a019b3f39667b
wangheng <wh9697yyk@163.com>
2020-05-29 09:35:57 +0800

    Solution1运行结果正确，但是超时

--- medium/decodeString.cpp
+++ medium/decodeString.cpp
@@ -11,38 +11,53 @@ using std::vector;
 using std::stoi;
 using std::reverse;
 
+// Solution1运行结果正确，但是超时
 class Solution1{
 public:
     string decodeString(string s) {
         string result;
         string elem;    // inout的每个元素
         int number;
+        int count = 0;
         vector<string> inout;
         for (char i : s) {
-            if (i != ']')
-                inout.emplace_back(i, 1);
+            if (i != ']') {
+                inout.emplace_back(1, i);
+                if (i == '[')
+                    ++count;
+            }
             else {
                 string leastString;
-                while ((elem = inout.back()) != "[") {
-                    leastString += elem;
+                while (count) {
+                    while ((elem = inout.back()) != "[") {
+                        leastString.append(elem);
+                        inout.pop_back();
+                    }
+                    --count;
+                    inout.pop_back();
+                    number = stoi(inout.back());
+                    inout.pop_back();
+                    string temp = leastString;
+                    while (--number)
+                        leastString.append(temp);
+                }
+                while (inout.size() > 0) {
+                    leastString.append(inout.back());
                     inout.pop_back();
                 }
-                inout.pop_back();
-                string n = inout.back();
-                number = stoi(n);
-                inout.pop_back();
                 reverse(leastString.begin(), leastString.end());
-                for (int j = 0; j < number; ++j)
-                    result += leastString;
+                result += leastString;
             }
         }
+        for (const string& iter : inout)
+            result += iter;
         return result;
     }
 };
 
 int main() {
     Solution1 solution;
-    std::cout << solution.decodeString("3[a2[c]]") << std::endl;
+    std::cout << solution.decodeString("ef3[c5[bg]]9[leetcode]mn") << std::endl;
 
     return 0;
 }
