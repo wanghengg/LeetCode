eacc331b59af2ed67ea8a41f1931ba65bf2f5176
wangheng <wh9697yyk@163.com>
2020-06-02 09:20:28 +0800

    2-Add Two Numbers解决

--- medium/2addTwoNumbers.cpp
+++ medium/2addTwoNumbers.cpp
@@ -13,44 +13,100 @@ struct ListNode {
 class Solution{
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* result;
-        ListNode* head = result;
+        ListNode* head1 = l1;
+        ListNode* head2 = l2;
         int carry = 0;
         int sum;
-        while (true) {
-            sum = l1->val + l2->val + carry;
+        while (head1->next != nullptr && head2->next != nullptr) {
+            sum = head1->val + head2->val + carry;
             if (sum > 9) {
-                head->val = sum % 10;
+                head1->val = sum % 10;
                 carry = 1;
             } else {
-                head->val = sum;
+                head1->val = sum;
                 carry = 0;
             }
-            if (l1->next == nullptr && l2->next == nullptr)
-                break;
-            if (l1->next != nullptr)
-                l1 = l1->next;
-            else
-                l1->val = 0;
-            if (l2->next != nullptr)
-                l2 = l2->next;
-            else
-                l2->val = 0;
+            head1 = head1->next;
+            head2 = head2->next;
         }
-//        if (getLength(l1) >= getLength(l2))
-//            head = l1;
-//        else
-//            head = l2;
-    }
+        if (head1->next == nullptr && head2->next != nullptr) {
+            head1->next = head2->next;
+            sum = head1->val + head2->val + carry;
+            if (sum > 9) {
+                head1->val = sum % 10;
+                carry = 1;
+            } else {
+                head1->val = sum;
+                carry = 0;
+            }
+            while (head1->next != nullptr) {
+                head1 = head1->next;
+                if ((head1->val + carry) > 9) {
+                    head1->val = 0;
+                    carry = 1;
+                } else {
+                    head1->val = head1->val + carry;
+                    carry = 0;
+                    break;
+                }
+            }
+            if (carry == 1)
+                head1->next = new ListNode(carry);
+            return l1;
+        }
 
-//    int getLength(ListNode* l) {
-//        int length = 0;
-//        while (l->next != nullptr)
-//            ++length;
-//        return ++length;
-//    }
+        if (head1->next != nullptr && head2->next == nullptr) {
+            sum = head1->val + head2->val + carry;
+            if (sum > 9) {
+                head1->val = sum % 10;
+                carry = 1;
+            } else {
+                head1->val = sum;
+                carry = 0;
+            }
+            while (head1->next != nullptr) {
+                head1 = head1->next;
+                if ((head1->val + carry) > 9) {
+                    head1->val = 0;
+                    carry = 1;
+                } else {
+                    head1->val = head1->val + carry;
+                    carry = 0;
+                    break;
+                }
+            }
+            if (carry == 1)
+                head1->next = new ListNode(carry);
+            return l1;
+        }
+
+        if (head1->next == nullptr && head2->next == nullptr) {
+            sum = head1->val + head2->val + carry;
+            if (sum > 9) {
+                head1->val = sum % 10;
+                carry = 1;
+            } else {
+                head1->val = sum;
+                carry = 0;
+            }
+            if (carry == 1)
+                head1->next = new ListNode(carry);
+            return l1;
+        }
+        return l1;
+    }
 };
 
 int main() {
+    Solution solution;
+    ListNode* l1 = new ListNode(9);
+    l1->next = new ListNode(8);
+    ListNode* l2 = new ListNode(1);
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+    while (result != nullptr) {
+        std::cout << result->val << ' ';
+        result = result->next;
+    }
 
+    return 0;
 }
