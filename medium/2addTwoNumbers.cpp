0c807f6416b7fe3c4a55a3d23b9e8f2f57ffd38d
wangheng <wh9697yyk@163.com>
2020-06-02 09:27:19 +0800

    2-Add Two Numbers添加了一些注释

--- medium/2addTwoNumbers.cpp
+++ medium/2addTwoNumbers.cpp
@@ -10,6 +10,7 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
+// 时间复杂度为O(n)，只进行一次扫描
 class Solution{
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -18,6 +19,7 @@ public:
         int carry = 0;
         int sum;
         while (head1->next != nullptr && head2->next != nullptr) {
+            // 最终返回l1,用l1表示结果
             sum = head1->val + head2->val + carry;
             if (sum > 9) {
                 head1->val = sum % 10;
@@ -29,6 +31,7 @@ public:
             head1 = head1->next;
             head2 = head2->next;
         }
+        // 如果l1比l2短，用head1->next = head2->next将l2长出来的部分接在l1之后
         if (head1->next == nullptr && head2->next != nullptr) {
             head1->next = head2->next;
             sum = head1->val + head2->val + carry;
@@ -47,9 +50,12 @@ public:
                 } else {
                     head1->val = head1->val + carry;
                     carry = 0;
+                    // 在长出来的部分，如果某一位不进位，那么其后的每一位都不会进位，所以break
                     break;
                 }
             }
+            // 如果所有的加法做完之后，carry=1，说明最末位进位，需要增加一个节点，使其值为1，
+            // 连接在末节点之后
             if (carry == 1)
                 head1->next = new ListNode(carry);
             return l1;
