e495961425d2e536505bacee93a91f0b21398fea
wangheng <wh9697yyk@163.com>
2020-10-06 22:03:39 +0800

    834

---
+++ hard/834sumOfDistancesInTree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
+
+    }
+};
+
+int main() {
+    Solution solution;
+    return 0;
+}
