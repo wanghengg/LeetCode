882b7aad8ee939b312fe552f0f0c4e577012aa75
wangheng <wh9697yyk@163.com>
2020-10-20 00:09:42 +0800

    jianzhioffer14-添加非递归解法

--- jianzhioffer/16myPow.cpp
+++ jianzhioffer/16myPow.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// 递归解法
 class Solution{
 public:
     double myPow(double x, int n) {
@@ -15,8 +16,29 @@ public:
     }
 };
 
+// 非递归迭代计算方法
+class Solution1{
+public:
+    double myPow(double x, int n) {
+        if (x == 0) return 0;
+        long b = n;
+        if (b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        if (n == 0) return 1;
+        double res = 1.0;
+        while (b > 0) {
+            if ((b & 1) == 1) res *= x;
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+};
+
 int main() {
-    Solution solution;
+    Solution1 solution;
     cout << solution.myPow(2.0, 10) << endl;
 
     return 0;
