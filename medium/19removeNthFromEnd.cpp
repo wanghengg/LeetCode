8e491832e345a6c9739a86f381bac879ce188c59
wangheng <wh9697yyk@163.com>
2020-10-25 22:36:26 +0800

    19-双指针删除链表倒数第N个节点

--- medium/19removeNthFromEnd.cpp
+++ medium/19removeNthFromEnd.cpp
@@ -1,26 +1,24 @@
-//
-// Created by wangheng on 2020/6/1.
-//
-
 #include <iostream>
 using namespace std;
 
 struct ListNode{
     int val;
     ListNode* next;
-    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
-// 双指针
 class Solution{
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* first = head, *second = head;
-        while (n--)
+        ListNode* first = head;
+        ListNode* second = head;
+        while (n--) {
             second = second->next;
-        if (second == nullptr)
-            return head->next;
-        while (second->next != nullptr) {
+        }
+        if (second == nullptr) return head->next;
+        while (second->next) {
             first = first->next;
             second = second->next;
         }
