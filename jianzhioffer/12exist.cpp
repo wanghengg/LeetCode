d65050babc410a726baca308360f5b4b6c46770a
wangheng <wh9697yyk@163.com>
2021-03-06 01:25:17 +0800

    深度优先搜索

---
+++ jianzhioffer/12exist.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        rows = board.size();
+        columns = board[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    int rows, columns;
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int k) {
+        if (i >= rows || i < 0 || j >= columns || j < 0 || board[i][j] != word[k])
+            return false;
+        if (k == word.size() - 1) return true;
+        board[i][j] = '\0';
+        bool res = dfs(board, word, i+1, j, k+1) ||
+                dfs(board, word, i-1, j, k+1) ||
+                dfs(board, word, i, j+1, k+1) ||
+                dfs(board, word, i, j-1, k+1);
+        board[i][j] = word[k];
+        return res;
+    }
+};
