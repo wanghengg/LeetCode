41caaa574d4d8dac3412a93c15135d0bee78bceb
wangheng <wh9697yyk@163.com>
2021-03-13 23:18:40 +0800

    集合划分

--- medium/0416canPartition.cpp
+++ medium/0416canPartition.cpp
@@ -1,4 +1,14 @@
-//
-// Created by wangheng on 2021/3/13.
-//
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
 
+class Solution{
+public:
+    bool canPartition(vector<int>& nums) {
+        if (nums.size() == 1) return false;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 == 1) return false;
+
+    }
+};
