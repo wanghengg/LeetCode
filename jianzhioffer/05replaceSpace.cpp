2b4a03f7c522735b3e4796d334a704351d333209
wangheng <wh9697yyk@163.com>
2021-03-28 14:41:31 +0800

    替换字符串中的空格

--- jianzhioffer/05replaceSpace.cpp
+++ jianzhioffer/05replaceSpace.cpp
@@ -15,6 +15,29 @@ public:
         }
         return result;
     }
+
+    // 原地替换
+    string replaceSpace(string s) {
+        // 先计算字符串中空格的个数
+        int count = 0;
+        int length = s.size();
+        for (int i = 0; i < length; ++i) {
+            if (s[i] == ' ') {
+                ++count;
+            }
+        }
+        s.resize(length + count * 2);
+        for (int i = length - 1, j = s.size() - 1; i >= 0; --i) {
+            if (s[i] == ' ') {
+                s[j--] = '0';
+                s[j--] = '2';
+                s[j--] = '%';
+            } else {
+                s[j--] = s[i];
+            }
+        }
+        return s;
+    }
 };
 
 int main() {
