7b5ac9a6606810b4539a669a93680d0ebd7b7a4a
wangheng <wh9697yyk@163.com>
2021-03-28 17:07:37 +0800

    根据前序遍历和中序遍历重建二叉树

--- jianzhioffer/07buildTree.cpp
+++ jianzhioffer/07buildTree.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 struct TreeNode{
@@ -11,11 +11,34 @@ struct TreeNode{
 };
 
 class Solution{
+private:
+    unordered_map<int, int> index;  // 方便查找中序遍历根节点的索引
 public:
+    TreeNode* myBuildTree(vector<int>& preorder, vector<int>& inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return nullptr;
+        }
+        // 前序遍历的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = index[preorder[preorder_root]];
+
+        // 先把根节点建立起来
+        TreeNode* root = new TreeNode(preorder[preorder_root]);
+        // 得到左右子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        root->left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        root->right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right  , inorder_root + 1, inorder_right);
+        return root;
+    }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        TreeNode* root = new TreeNode(preorder[0]);
-        auto index = find(inorder.begin(), inorder.end(), preorder[0]);
-        vector<int> left = vector<int>(inorder.begin(), index);
-        vector<int> right = vector<int>(index, inorder.end());
+        int n = preorder.size();
+        for (int i = 0; i < n; ++i) {
+            index[inorder[i]] = i;
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
     }
 };
