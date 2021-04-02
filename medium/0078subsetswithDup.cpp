7c9e32ce019d736c24c29436e2c34359eb1d7ce1
wangheng <wh9697yyk@163.com>
2021-04-02 15:43:04 +0800

    子集

--- medium/0078subsetswithDup.cpp
+++ medium/0078subsetswithDup.cpp
@@ -24,3 +24,22 @@ public:
         return res;
     }
 };
+
+class Solution1{
+public:
+    vector<vector<int>> result;
+    vector<int> t;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        for (int mask = 0; mask < (1 << n); ++mask) {
+            t.clear();
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    t.emplace_back(nums[i]);
+                }
+            }
+            result.emplace_back(t);
+        }
+        return result;
+    }
+};
