df9d63172bb799ada1e88d7cc43d58af32f49ae9
wangheng <wh9697yyk@163.com>
2021-04-04 12:09:02 +0800

    贪心算法删除k位数字保证剩余数字最小

---
+++ medium/0402removeKdigits.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    /*
+     * 从高位往低位删除数字
+     */
+    string removeKdigits(string num, int k) {
+        if (num.size() == k) return "0";
+        while (!num.empty() && k--) {
+            if (num.size() == 1) {
+                num.erase(0, 1);
+            } else {
+                int index = 1;
+                while (num[index] >= num[index - 1]) {
+                    ++index;
+                }
+                num.erase(index-1, 1);
+                while (num[0] == '0') {
+                    num.erase(0, 1);
+                }
+            }
+        }
+        if (num.empty()) return "0";
+        return num;
+    }
+};
+
+int main() {
+    string num{"10"};
+    Solution solution;
+    cout << solution.removeKdigits(num, 2) << endl;
+    return 0;
+}
