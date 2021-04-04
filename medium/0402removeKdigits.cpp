7a70d439dd09bd1c89aab4136b85b46a39dcfa46
wangheng <wh9697yyk@163.com>
2021-04-04 12:54:47 +0800

    用string模拟单调栈，时间复杂度O(n)

--- medium/0402removeKdigits.cpp
+++ medium/0402removeKdigits.cpp
@@ -27,9 +27,35 @@ public:
     }
 };
 
+/*
+ * 第一种解法的时间复杂度是O(kn)
+ * 通过一个单调栈可以降低时间复杂度至O(n)，用string模拟单调栈
+ */
+
+class Solution1{
+public:
+    string removeKdigits(string num, int k) {
+        string result;
+        for (auto& iter : num) {
+            while (!result.empty() && result.back() > iter && k) {
+                result.pop_back();
+                --k;
+            }
+            if (result.empty() && iter == '0')  // 此时栈为空，且要添加的数字为'0'，即前导0，不应该加入
+                continue;
+            result.push_back(iter);
+        }
+        while (k > 0 && !result.empty()) {
+            result.pop_back();
+            --k;
+        }
+        return result.empty() ? "0" : result;
+    }
+};
+
 int main() {
     string num{"123"};
-    Solution solution;
+    Solution1 solution;
     cout << solution.removeKdigits(num, 2) << endl;
     return 0;
 }
