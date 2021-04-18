1ed557b270feccca274ed0925c19d6d0ac47cfd8
wangheng <wh9697yyk@163.com>
2021-04-18 21:09:58 +0800

    tag:位运算-不用加减乘除做加法

--- jianzhioffer/65add.cpp
+++ jianzhioffer/65add.cpp
@@ -11,6 +11,7 @@ public:
         if (b == 0) {
             return a;
         }
+        // a^b是非进位和（不包含进位），(a&b)<<1是进位，对负数不能直接移位，所以应该先强制类型转换为无符号数
         return add(a ^ b, (unsigned int)(a & b) << 1);
     }
 };
