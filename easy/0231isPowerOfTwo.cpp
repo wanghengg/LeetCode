cc3ee5dd8411967f834bedd14e31197039881dfe
wangheng <wh9697yyk@163.com>
2021-05-31 13:12:22 +0800

    tag:位运算——计算数组元素汉明距离总和

--- easy/0231isPowerOfTwo.cpp
+++ easy/0231isPowerOfTwo.cpp
@@ -17,8 +17,20 @@ public:
     }
 };
 
+/*
+ * 如果一个数是2的幂，那么它的二进制一定只有一个1，并且其为正整数
+ * 判断一个数的二进制是否只有一个1的办法，n&(n-1)可以去掉最低位的1，如果去掉最低位的1
+ * 后值为0说明这个数的二进制只有一个1
+ */
+class Solution1{
+public:
+    bool isPowerOfTwo(int n) {
+        return (n > 0 && (n & (n-1)) == 0);
+    }
+};
+
 int main() {
-    Solution solution;
+    Solution1 solution;
     int n = 4;
     cout << boolalpha << solution.isPowerOfTwo(n) << endl;
     return 0;
