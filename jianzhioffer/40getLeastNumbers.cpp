786b968522a68206c7412f41062b1b42f6ff7933
wangheng <wh9697yyk@163.com>
2021-06-11 18:08:19 +0800

    tag:堆——最小的K个数

--- jianzhioffer/40getLeastNumbers.cpp
+++ jianzhioffer/40getLeastNumbers.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 class Solution {
@@ -10,6 +11,31 @@ public:
         vector<int> res(k, 0);
         for (int i = 0; i < k; ++i) {
             res[i] = arr[i];
+        }
+        return res;
+    }
+};
+
+class Solution1 {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        vector<int> res(k, 0);
+        if (k == 0) return res;
+        priority_queue<int> q;
+        for (int i = 0; i < k; ++i) {
+            q.emplace(arr[i]);
+        }
+
+        for (int i = k; i < arr.size(); ++i) {
+            if (q.top() > arr[i]) {
+                q.pop();
+                q.push(arr[i]);
+            }
+        }
+
+        for (int i = 0; i < k; ++i) {
+            res[i] = q.top();
+            q.pop();
         }
         return res;
     }
