8f519b115571bd6a76b0b758edaf4c98d68f8b4f
wangheng <wh9697yyk@163.com>
2021-06-13 23:31:49 +0800

    tag:二分查找——添加一种新解法并添加注释

--- easy/0278firstBadVersion.cpp
+++ easy/0278firstBadVersion.cpp
@@ -16,11 +16,27 @@ public:
         while (left < right) {
             int mid = (right - left) / 2 + left;
             if (isBadVersion(mid)) {
-                right = mid - 1;
+                right = mid - 1;    // 与普通二分查找找到相应元素直接返回不同，这里要查第一次满足条件的元素，所以查到之后不能返回，而是继续二分
             } else {
                 left = mid + 1;
             }
         }
         return isBadVersion(left) ? left : left + 1;
+    }
+};
+
+class Solution1{
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 };
